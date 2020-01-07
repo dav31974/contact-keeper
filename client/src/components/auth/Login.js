cc8d9ed609d45aef991a7ad2dd0ff9b6578cac6c
david <davidstephane31@gmail.com>
2020-01-07 16:23:21 +0100

    login and register form

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = user;
+
+  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log('Lgin submit');
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>
+        Account <span className='text-primary'>Login</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Adress</label>
+          <input
+            type='text'
+            name='email'
+            value={email}
+            placeholder='Enter your email adress'
+            onChange={onChange}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            placeholder='Enter your password'
+            onChange={onChange}
+          />
+        </div>
+        <input
+          type='submit'
+          value='Login'
+          className='btn btn-primary btn-block'
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
